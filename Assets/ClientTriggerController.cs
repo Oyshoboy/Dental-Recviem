66864102f6dd0a110b5e8a1f3a220466de3afc06
Oleg <oyshoboy@gmail.com>
2020-10-04 06:09:24 -0700

    Doctor Hands controller done

--- Assets/ClientTriggerController.cs
+++ Assets/ClientTriggerController.cs
@@ -2,17 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using Dreamteck.Splines;
+using RootMotion.FinalIK;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using Random = System.Random;
 
 public class ClientTriggerController : MonoBehaviour
 {
+    [Header("Playa Refferences")]
     public GameObject playa;
+    public Animator playaAnimator;
+    public SkinnedMeshRenderer playaHead;
+    public FBBIKHeadEffector playaHeadEffector;
+    
+    [Header("Playa Sit Config")]
     public float timeToTurn = 3f;
     public float idleTime = 3f;
-    public GameObject directionPoint;
-    public Animator playaAnimator;
+    
+    [Header("Playa Directions")]
+    public Transform playaTurnDirection;
+    public Transform playaSitDirection;
+
+    [Header("Doctor Config")]
+    public Doctor_Controller doctorController;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "PlayaCollider")
@@ -31,23 +44,56 @@ public class ClientTriggerController : MonoBehaviour
         var time = 0f;
         while (time < 1)
         {
-            time += Time.deltaTime / timeToTurn;
+            time += Time.deltaTime;
             TurnLeft(time);
+            
             yield return null;
         }
 
         PlayerSit();
+        time = 0f;
+        while (time < 1)
+        {
+            time += Time.deltaTime;
+            MoveToSitPosition(time);
+            
+            yield return null;
+        }
+        
+        time = 0f;
+        while (time < 1)
+        {
+            time += Time.deltaTime;
+            OpenMouthAndMoveHead(time);
+            
+            yield return null;
+        }
+
+        doctorController.StartCoroutine(nameof(Doctor_Controller.StartMovingHands), true);
+    }
+
+    private void OpenMouthAndMoveHead(float value)
+    {
+        playaHead.SetBlendShapeWeight(34, value * 100); // MOUTH WITHD
+        playaHead.SetBlendShapeWeight(36, value * 100); // MOUTH OPEN
+
+        playaHeadEffector.positionWeight = value;
+        playaHeadEffector.rotationWeight = value;
     }
 
     private void PlayerSit()
     {
-        playaAnimator.applyRootMotion = true;
         playaAnimator.SetTrigger("sit");
     }
 
     void TurnLeft(float time)
     {
-        playa.transform.rotation = Quaternion.Lerp(playa.transform.rotation, directionPoint.transform.rotation, time);
+        playa.transform.rotation = Quaternion.Lerp(playa.transform.rotation, playaTurnDirection.transform.rotation, time);
     }
     
-}
+    void MoveToSitPosition(float time)
+    {
+        playa.transform.position = Vector3.Lerp(playa.transform.position, playaSitDirection.transform.position, time);
+    }
+    
+}// 34 36
---
+++ Assets/Doctor_Controller.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RootMotion.Demos;
+using UnityEngine;
+
+public class Doctor_Controller : MonoBehaviour
+{
+    public GameObject rightHand;
+    public Vector3 rightHandDefaultPos;
+    private Quaternion rightHandDefaultRot;
+    public Transform rightHandTargetPos;
+
+    public GameObject leftHand;
+    public Vector3 leftHandDefaultPos;
+    private Quaternion leftHandDefaultRot;
+    public Transform LeftHandTargetPos;
+    
+    public GameObject head;
+    public Vector3 headDefaultPos;
+    public Transform headTargetPos;
+    
+    private void Start()
+    {
+        InitDefaultPost();
+    }
+
+    void InitDefaultPost()
+    {
+        rightHandDefaultPos = rightHand.transform.localPosition;
+        rightHandDefaultRot = rightHand.transform.localRotation;
+        
+        leftHandDefaultPos = leftHand.transform.localPosition;
+        leftHandDefaultRot = leftHand.transform.localRotation;
+
+        headDefaultPos = head.transform.localPosition;
+    }
+
+    public IEnumerator StartMovingHands(bool forward)
+    {
+        var time = 0f;
+        while (time < 1)
+        {
+            time += Time.deltaTime;
+            if (forward)
+            {
+                HandsMovementController(time);
+                HeadMovementController(time);
+            }
+            else
+            {
+                HandsReturnController(time);
+                HeadReturnController(time);
+            }
+
+            yield return null;
+        }
+    }
+
+    void HeadMovementController(float time)
+    {
+        head.transform.localPosition = Vector3.Lerp(headDefaultPos, headTargetPos.localPosition, time);
+    }
+    
+    void HeadReturnController(float time)
+    {
+        head.transform.localPosition = Vector3.Lerp(headTargetPos.localPosition, headDefaultPos, time);
+    }
+
+    void HandsMovementController(float time)
+    {
+        rightHand.transform.localPosition = Vector3.Lerp(rightHandDefaultPos, rightHandTargetPos.localPosition, time);
+        rightHand.transform.localRotation = Quaternion.Lerp(rightHandDefaultRot, rightHandTargetPos.localRotation, time);
+        
+        leftHand.transform.localPosition = Vector3.Lerp(leftHandDefaultPos, LeftHandTargetPos.localPosition, time);
+        leftHand.transform.localRotation = Quaternion.Lerp(leftHandDefaultRot, LeftHandTargetPos.localRotation, time);
+    }
+
+    void HandsReturnController(float time)
+    {
+        rightHand.transform.localPosition = Vector3.Lerp(rightHandTargetPos.localPosition, rightHandDefaultPos, time);
+        rightHand.transform.localRotation = Quaternion.Lerp(rightHandTargetPos.localRotation, rightHandDefaultRot, time);
+        
+        leftHand.transform.localPosition = Vector3.Lerp(LeftHandTargetPos.localPosition, leftHandDefaultPos, time);
+        leftHand.transform.localRotation = Quaternion.Lerp(LeftHandTargetPos.localRotation, leftHandDefaultRot, time);
+    }
+}
