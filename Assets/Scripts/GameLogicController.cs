cc31d7d71e2e9f8b399f7e658269a102729db066
Oleg <oyshoboy@gmail.com>
2020-10-04 19:32:09 -0700

    Imported particles + Made tooth spawning stuff :D

--- Assets/Scripts/GameLogicController.cs
+++ Assets/Scripts/GameLogicController.cs
@@ -1,14 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using FIMSpace.Jiggling;
 using UnityEngine;
 
 public class GameLogicController : MonoBehaviour
 {
+    public GameObject upperJaw;
+    public List<GameObject> upperTeeth;
+    public List<int> upperShrinked;
+    
+    public GameObject lowerJaw;
+    public List<GameObject> lowerTeeth;
+    public List<int> lowerShrinked;
+
+    public ToothPlateSpawner toothPlateSpawner;
+
+    List<GameObject> ChildExtractor(GameObject objct, List<GameObject> tempList)
+    {
+        int children = objct.transform.childCount;
+        for (int i = 0; i < children; ++i)
+        {
+            tempList.Add(objct.transform.GetChild(i).gameObject);
+        }
+
+        return tempList;
+    }
+    
+    void InitTeethFromJaws()
+    {
+        upperTeeth = ChildExtractor(upperJaw, upperTeeth);
+        lowerTeeth = ChildExtractor(lowerJaw, lowerTeeth);
+    }
+
+    void RandomShrinker(List<GameObject> list, bool upper)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (Random.Range(0, 5) > 1)
+            {
+                if (upper)
+                {
+                    upperShrinked.Add(i);
+                }
+                else
+                {
+                    lowerShrinked.Add(i);
+                }
+                list[i].GetComponent<FJiggling_Grow>().StartShrinking();
+            }
+        }
+    }
+    void InitRandomTeethRemoved()
+    {
+        RandomShrinker(upperTeeth, true);
+        RandomShrinker(lowerTeeth, false);
+    }
+    
+    void InitEverything()
+    {
+        InitTeethFromJaws();
+        InitRandomTeethRemoved();
+        
+        toothPlateSpawner.SpawnToothOnPlate(upperShrinked.Count + lowerShrinked.Count);   
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitEverything();
     }
+    
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/ToothPlateSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothPlateSpawner : MonoBehaviour
+{
+    public float radius = 1;
+    public Vector2 regionSize = Vector2.one;
+    public int rejectionSamples = 30;
+    public float displayRadius = 1f; 
+    // 0 FOR UNLIMITED
+    
+    private List<Vector2> points;
+
+    public GameObject doctorsPlate;
+    public GameObject[] toothesPool;
+
+    public void SpawnToothOnPlate(int samplesAmount)
+    {
+        points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples, samplesAmount);
+
+        int foreachIndex = 0;
+
+        for (int i = 0; i < toothesPool.Length; i++)
+        {
+            toothesPool[i].SetActive(false);
+        }
+        
+        if (points != null)
+        {
+            foreach (Vector2 point in points)
+            {
+                toothesPool[foreachIndex].transform.localPosition = new Vector3(doctorsPlate.transform.localPosition.x + point.x * 10, doctorsPlate.transform.localPosition.y + point.y * 10, -0.054f);
+                toothesPool[foreachIndex].SetActive(true);
+                foreachIndex++;
+            }
+        }
+    }
+}
